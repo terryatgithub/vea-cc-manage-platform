f3296594a6537cbf576592a85d00b3ad68bab8a3
liqi <liqi@coocaa.com>
2019-03-20 20:07:06 +0800

    系统设置角色管理的新增，编辑保存时数据格式修改

--- src/services/systemSetting/roleManage.js
+++ src/services/systemSetting/roleManage.js
@@ -1,6 +1,6 @@
 /**
  *
- * 角色管理接口
+ * 角色管理接口 当参数是data的时候，传的是Body里面的内容格式，当参数是params时，这是拼接到头部url地址栏里的
  */
 export function getRoleList (data) {
   return this.fetch({
@@ -9,18 +9,18 @@ export function getRoleList (data) {
     data
   })
 }
-export function userRoleList (data) {
+export function userRoleList (params) {
   return this.fetch({
     method: 'post',
     url: '/api/sysRole/userList.html',
-    data
+    params
   })
 }
-export function saveRoleData (params) {
+export function saveRoleData (data) {
   return this.fetch({
     method: 'post',
     url: '/api/sysRole/save.html',
-    params
+    data
   })
 }
 export function deleteRole (params) {
