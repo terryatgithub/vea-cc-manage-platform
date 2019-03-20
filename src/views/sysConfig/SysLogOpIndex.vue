19b35b62cedfb4337774acf384e92f24209b7837
yudefu <yudefu@coocaa.com>
2019-03-20 15:29:32 +0800

    系统设置->操作日志

--- src/views/sysConfig/SysLogOpIndex.vue
+++ src/views/sysConfig/SysLogOpIndex.vue
@@ -1,14 +1,44 @@
 <template>
   <ContentCard class="content">
     <ContentWrapper
-      :filter="filter"
-      :filterSchema="filterSchema"
       :pagination="pagination"
       @filter-change="handleFilterChange"
-      @filter-reset="handleFilterReset"
     >
+      <el-form ref="form" :model="filter" inline label-width="90px" size="mini">
+        <el-form-item label="自增ID">
+          <el-input v-model="filter.id"></el-input>
+        </el-form-item>
+        <el-form-item label="操作者ID">
+          <el-input v-model="filter.userId"></el-input>
+        </el-form-item>
+        <el-form-item label="操作者名称">
+          <el-input v-model="filter.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="菜单名称">
+          <el-input v-model="filter.menuName"></el-input>
+        </el-form-item>
+        <el-form-item label="菜单Elid">
+          <el-input v-model="filter.menuElid"></el-input>
+        </el-form-item>
+        <el-form-item label="执行的方法">
+          <el-input v-model="filter.execMethod"></el-input>
+        </el-form-item>
+        <el-form-item label="操作耗时">
+          <el-input v-model="filter.elapsedTime"></el-input>
+        </el-form-item>
+        <el-form-item label="操作时间">
+          <el-input v-model="filter.createTime"></el-input>
+        </el-form-item>
+        <el-form-item label="操作的数据">
+          <el-input v-model="filter.opData" type="textarea" size="medium"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" icon="el-icon-search" @click="handleFilterChange">搜索</el-button>
+          <el-button icon="el-icon-delete" @click="clearForm">清空</el-button>
+        </el-form-item>
+      </el-form>
+
       <Table
-        :cell-style="cellStyle"
         :props="table.props"
         :header="table.header"
         :data="table.data"
@@ -22,6 +52,7 @@
 <script>
 import { ContentWrapper, Table, utils } from 'admin-toolkit'
 import _ from '../../utlis/gateschema'
+import { Button, Tooltip } from 'element-ui'
 
 export default {
   components: {
@@ -29,60 +60,66 @@ export default {
     Table
   },
 
-  data () {
+  data() {
     return {
-      filterSchema: null,
       filter: {
         sort: undefined,
         order: undefined
       },
-      pagination: {},
       selected: [],
+      pagination: {},
       table: {
         props: {},
         header: [
           {
             label: '自增ID',
             prop: 'id',
-            fit: true
+            width: '70',
+            sortable: true
           },
           {
             label: '操作者ID',
             prop: 'userId',
-            fit: true
+            width: '60',
+            sortable: true
           },
           {
             label: '操作名称',
-            prop: 'userName',
-            fit: true
+            prop: 'userName'
           },
           {
             label: '菜单名称',
-            prop: 'menuName',
-            fit: true
+            prop: 'menuName'
           },
           {
             label: '菜单Elid',
-            prop: 'menuElid',
-            fit: true
+            prop: 'menuElid'
           },
           {
             label: '执行的方法',
-            prop: 'execMethod',
-            fit: true
+            prop: 'execMethod'
           },
           {
             label: '操作的数据',
             prop: 'opData',
-            fit: true,
-            // render: utils.component.createOperationRender(this, {
-            //   showOpData: 'data'
-            // })
+            render(h, { row }) {
+              return h(
+                Tooltip,
+                {
+                  props: {
+                    content: row.opData,
+                    placement: 'top'
+                  }
+                },
+                [h('div', { class: 'op-data' }, [row.opData])]
+              )
+            }
           },
           {
             label: '操作耗时',
             prop: 'elapsedTime',
-            fit: true
+            fit: true,
+            sortable: true
           },
           {
             label: '操作时间',
@@ -91,32 +128,46 @@ export default {
           },
           {
             label: '操作',
-            width: '60',
+            width: '90',
             fixed: 'right',
-            render: utils.component.createOperationRender(this, {
-              editData: '复制'
-            })
+            render(h, { row }) {
+              return h(
+                Button,
+                {
+                  props: {
+                    type: 'success',
+                    icon: 'el-icon-plus'
+                  },
+                  on: {
+                    click: function() {
+                      console.log(this)
+                    }
+                  }
+                },
+                ['复制']
+              )
+            }
           }
         ],
         data: [],
         selected: [],
         selectionType: 'none'
       }
-    };
+    }
   },
 
   methods: {
     /**
      * 获取数据
      */
-    fetchData () {
+    fetchData() {
       const filter = this.parseFilter()
       this.$service.sysLogOpPageList(filter).then(data => {
         this.pagination.total = data.total
         this.table.data = data.rows
       })
     },
-    parseFilter () {
+    parseFilter() {
       const { filter, pagination } = this
       if (pagination) {
         filter.page = pagination.currentPage
@@ -124,80 +175,26 @@ export default {
       }
       return filter
     },
-    // 表单重设
-    handleFilterReset () {
-      this.filter = {
-        sort: undefined,
-        order: undefined
-      }
+    clearForm() {
+      this.filter = {}
       this.fetchData()
     },
-    handleFilterChange (type) {
+    handleFilterChange(type) {
       if (type === 'filter') {
         if (this.pagination) {
           this.pagination.currentPage = 1
         }
       }
       this.fetchData()
-    },
-    cellStyle (row) {
-      return {
-        'height': '50px'
-      }
     }
   },
   created() {
-    const filterSchema = _.map({
-      id: _.o.number.other("form", {
-        label: "自增ID"
-      }),
-      userId: _.o.number.other("form", {
-        label: "操作者ID"
-      }),
-      userName: _.o.string.other("form", {
-        label: "操作者名称"
-      }),
-      menuName: _.o.string.other("form", {
-        label: "菜单名称"
-      }),
-      menuElid: _.o.string.other("form", {
-        label: "菜单Elid"
-      }),
-      execMethod: _.o.string.other("form", {
-        label: "执行的方法"
-      }),
-      opData: _.o.string.other("form", {
-        label: "操作的数据"
-      }),
-      elapsedTime: _.o.number.other("form", {
-        label: "操作耗时"
-      }),
-      createTime: _.o.string.other("form", {
-        label: "操作时间"
-      }),
-    }).other("form", {
-      cols: {
-        item: 10,
-        wrapper: 10,
-        label: 3
-      },
-      footer: {
-        cols: {
-          label: 0,
-          wrapper: 24
-        },
-        showSubmit: true,
-        submitText: '查询',
-        showReset: true,
-        resetText: '重置'
-      }
-    })
-    this.filterSchema = filterSchema
     this.fetchData()
   }
-
 }
 </script>
 
 <style lang='stylus' scoped>
+.content >>> .op-data
+  height: 50px;
 </style>
