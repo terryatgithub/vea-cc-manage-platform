876fa54dc4529dd0f8a33683a5db67b8fc05c9e7
392241868 <392241868@qq.com>
2019-04-13 09:30:46 +0800

    版块管理合并

--- src/services/blockManage/panelInfo.js
+++ src/services/blockManage/panelInfo.js
@@ -4,7 +4,6 @@
 export function panelPageList(data) {
   return this.fetch({
     method: 'post',
-<<<<<<< HEAD
     url: '/api/v1/panel/pageList.html',
     data
   })
@@ -14,9 +13,6 @@ export function panelDataList(data) {
   return this.fetch({
     method: 'post',
     url: '/api/v1/panel/dataList.html',
-=======
-    url: 'api/panel/pageList.html',
->>>>>>> f8a755de70512085bcd651f80725dcb32bee43a6
     data
   })
 }
