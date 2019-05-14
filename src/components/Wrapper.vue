02a014b0cb3bc54c061b4d2b17d4ceeb5271ddb2
LiangWeiliang <liangweiliang@coocaa.com>
2019-05-14 16:04:33 +0800

    fix:解决标签状态问题

--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -1,5 +1,19 @@
 <template>
 <div>
-  <router-view />
+  <keep-alive>
+    <router-view v-if="isKeepAlive"/>
+  </keep-alive>
+  <router-view v-if="!isKeepAlive"/>
 </div>
 </template>
+<script>
+export default {
+  computed: {
+    isKeepAlive() {
+      const meta = this.$route.meta
+      return meta && meta.isCache !== false
+    }
+  }
+}
+</script>
+
