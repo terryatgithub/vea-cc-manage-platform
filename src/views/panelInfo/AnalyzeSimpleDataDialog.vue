53810eb07e5576e12ac54aca859da15881e7a761
yudefu <yudefu@coocaa.com>
2019-09-25 20:41:26 +0800

    开发后台数据展现优化需求

---
+++ src/views/panelInfo/AnalyzeSimpleDataDialog.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <el-dialog title="看数据"
+      :visible.sync="visible"
+      :show="show"
+      width="70%"
+      @close="$emit('update:show', false)"
+      @open="fetchData"
+    >
+      1111
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data() {
+      return {
+        visible: this.show
+      }
+    },
+
+    watch: {
+      show () {
+        this.visible = this.show
+      }
+    },
+
+    props: {
+      show: {
+        type: Boolean,
+        default () {
+          return false
+        }
+      }
+    },
+
+    methods: {
+      fetchData () {
+
+      }
+    },
+
+    created() {},
+  }
+</script>
+
+<style lang="stylus" scoped></style>
