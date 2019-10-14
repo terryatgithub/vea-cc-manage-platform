3326d41ac35c56e75dbbeb8c40ed6894c95e4a13
liangweiliang <superbill2012@qq.com>
2019-10-14 15:05:17 +0800

    feat:版面id选择组件

---
+++ src/components/TabIdSelect.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-select
+    :value="value"
+    @input="$emit('input', $event)"
+    filterable
+    remote
+    reserve-keyword
+    name="tabId"
+    autocomplete="on"
+    :placeholder="placeholder"
+    :remote-method="fetchData"
+    :loading="loading">
+    <el-option
+      v-for="item in tabList"
+      :key="item.tabId"
+      :label="item.tabCnTitle"
+      :value="item.tabId">
+    </el-option>
+  </el-select>  
+</template>
+<script>
+export default {
+  props: ['value', 'placeholder'],
+  data () {
+    return {
+      tab: null,
+      loading: false,
+      tabList: []
+    }
+  },
+  methods: {
+    fetchData () {
+      this.loading = true
+      this.$service.tabInfoGetListByKeyword().then(data => {
+        this.tabList = data.rows
+        this.loading = false
+      })
+    }
+  }
+}
+</script>
