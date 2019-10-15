1868875358cf2d12df03372bded6afee41b102f3
liangweiliang <superbill2012@qq.com>
2019-10-15 21:41:39 +0800

    fix:版块标题允许空格

--- src/views/panelInfo/PanelInfo.vue
+++ src/views/panelInfo/PanelInfo.vue
@@ -121,7 +121,7 @@
               <el-form-item v-if="pannel.parentType === 'normal'" label="版块标题" required>
                 <el-input
                   style="width: 300px"
-                  v-model.trim="pannel.pannelList[0].pannelTitle"
+                  v-model="pannel.pannelList[0].pannelTitle"
                   placeholder="请输入版块标题"
                 ></el-input>
                 <BinCheckBox label="前端不显示标题" v-model="pannel.showTitle" is-negative />
