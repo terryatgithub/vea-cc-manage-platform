9d2f7c32007574c40edf69b38226ea6e26aafaf2
liangweiliang <superbill2012@qq.com>
2019-12-13 16:41:25 +0800

    refactor:添加HOST开发配置环境变量

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,17 @@
 const ENV = process.env.NODE_ENV
+const HOST = process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || '8081'
 // dev地址172.20.155.101:8090,bate的地址是172.20.155.91:8090
 const BACKEND = process.env.BACKEND || '172.20.155.91:8090' // 172.20.155.91:8090 localhost:9090
 // const path = require('path')
 // const dist = path.resolve('./dist')
-const baseUrl = ENV === 'production' ? '' : 'http://127.0.0.1:8081'
+const baseUrl = ENV === 'production' ? '' : `http://${HOST}:${PORT}`
 
 module.exports = {
   publicPath: baseUrl,
   devServer: {
-    public: '127.0.0.1:8081',
-    port: '8081',
+    host: HOST,
+    port: PORT,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
