5384cde0b57f7ea8a5a6e4247b62836eb5fd6215
liangweiliang <superbill2012@qq.com>
2019-12-30 10:43:11 +0800

    refactor:更换测试环境后端地址

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const ENV = process.env.NODE_ENV
 const HOST = process.env.HOST || '127.0.0.1'
 const PORT = process.env.PORT || '8081'
 // dev地址172.20.155.101:8090,bate的地址是172.20.155.91:8090
-const BACKEND = process.env.BACKEND || '172.20.155.91:8090' // 172.20.155.91:8090 localhost:9090
+const BACKEND = process.env.BACKEND || '172.20.155.91:8222' // 172.20.155.91:8090 localhost:9090
 // const path = require('path')
 // const dist = path.resolve('./dist')
 const baseUrl = ENV === 'production' ? '' : `http://${HOST}:${PORT}`
