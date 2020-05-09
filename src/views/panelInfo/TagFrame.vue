4b9ddcc206cb558999991b67da8601e127a2d55c
liangweiliang <superbill2012@qq.com>
2020-05-09 16:43:47 +0800

    feat:打标签使用新媒资平台页面

--- src/views/panelInfo/TagFrame.vue
+++ src/views/panelInfo/TagFrame.vue
@@ -25,7 +25,7 @@ export default {
       const server = /^(127|172|beta)/.test(location.host)
         ? 'http://dev-mgr-media.tc.cc0808.com'
         : 'http://mgr.media.tc.skysrt.com'
-      return server + `/ccGlobalMediaTag/com.html?origin=${location.origin}&referrer=homepage&enableDelete=0&coocaaVId=` + ids.join(',')
+      return server + `/#/ccGlobalMediaTag/com.html?origin=${location.origin}&referrer=homepage&enableDelete=0&coocaaVId=` + ids.join(',')
     }
   },
   methods: {
