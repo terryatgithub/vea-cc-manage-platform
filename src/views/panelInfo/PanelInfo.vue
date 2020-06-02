ed47952df363e6ed70d99575200739fe0d378b0d
yudefu <yudefu@coocaa.com>
2020-06-02 16:27:10 +0800

    fix: 版块推荐位个数编辑初始化

--- src/views/panelInfo/PanelInfo.vue
+++ src/views/panelInfo/PanelInfo.vue
@@ -2965,6 +2965,7 @@ export default {
           this.blockCountList = pannelList.map(function (item) {
             return item.contentList.length
           })
+          this.blockCount = this.blockCountList[0] || 1
           pannel.pannelList = pannelList.map(function (item) {
             item.contentList = item.contentList.map(function (contentItem) {
               contentItem.contentPosition = JSON.parse(
