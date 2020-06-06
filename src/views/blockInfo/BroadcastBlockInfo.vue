9b79f7ba69d81e53915a120b3fdd16feaa02f620
lushanshan <lushanshan@coocaa.com>
2020-06-06 16:01:03 +0800

    保存未放开

--- src/views/blockInfo/BroadcastBlockInfo.vue
+++ src/views/blockInfo/BroadcastBlockInfo.vue
@@ -1378,11 +1378,11 @@ export default {
       lowerVersionContent.onclick = JSON.stringify(lowerVersionContent.onclick)
       data.parentType = 'Block'
       console.log('save', data)
-      // this.$service
-      //   .saveBlockInfo({ jsonStr: JSON.stringify(data) }, '提交成功')
-      //   .then(() => {
-      //     this.$emit('upsert-end')
-      //   })
+      this.$service
+        .saveBlockInfo({ jsonStr: JSON.stringify(data) }, '提交成功')
+        .then(() => {
+          this.$emit('upsert-end')
+        })
     },
 
     fetchData (version) {
